e6f259c8400823845d150608e0f735e008ad6552
Miikka Virtanen <32563239+MiiVirt@users.noreply.github.com>
2024-01-22 11:47:57 +0200

    Add files via upload
    Fixed issue with decoding.

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-import os, secrets, hashlib, base64
+import secrets, hashlib
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
 
 #TODO comments
-def generate_salt():
+def generate_random_16byte(): #Generate random (using secrets) 16 byte value
     return secrets.token_bytes(16)
 
 def generate_key(password, salt):
+    """
+    :param password: User given password
+    :param salt: Salt generated in generate_salt() used to add randomly generated data for added security
+    :return: Using PBKDF2-HMAC key derivation funtion (Kdf) with SHA-256 a key is derived from password and salt
+    """
     kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), salt=salt, iterations=100000, length=32)
     key = kdf.derive(password.encode('utf-8'))
     return key
 
 def hash_data(data, salt):
-    if isinstance(data, str):
-        data_bytes = data.encode('utf-8')
-    elif isinstance(data, bytes):
-        data_bytes = data
-    else:
-        raise ValueError("Unsupported data type")
-
+    data_bytes = data.encode('utf-8')
     data_with_salt = data_bytes + salt
     hashed_data = hashlib.sha256(data_with_salt).hexdigest()
     return hashed_data
 
 def encrypt_data(data, key, iv):
+    print("Data before encryption", data)
     cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     encryptor = cipher.encryptor()
-
-    if isinstance(data, str):
-        data = data.encode('utf-8')
-
+    data = data.encode('utf-8')
     encrypted_data = encryptor.update(data) + encryptor.finalize()
+    print("Encrypted data", encrypted_data)
     return encrypted_data
 
 def decrypt_data(encrypted_data, key, iv):
+    print("Encrypted data before decryption", encrypted_data)
     cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     decrypted_data = decryptor.update(encrypted_data) + decryptor.finalize()
@@ -44,23 +44,22 @@ def decrypt_data(encrypted_data, key, iv):
 def main():
     password = input("Password:")
     data_to_encrypt = input("Data:")
-    salt = generate_salt()
+    salt = generate_random_16byte()
     key = generate_key(password, salt)
-    iv = secrets.token_bytes(16)
+    iv = generate_random_16byte()
 
     hashed_data = hash_data(data_to_encrypt, salt)
     encrypted_data = encrypt_data(hashed_data, key, iv)
 
     print("Encryption was successful.")
 
-    decrypted_data = decrypt_data(encrypted_data, key, iv)
-    decrypted_data_hashed = hash_data(data_to_encrypt, salt)
+    decrypted_data = decrypt_data(encrypted_data, key, iv).decode('utf-8')
     print("Decryption was successfull.")
 
-    #print(hashed_data)
-    #print(decrypted_data_hashed)
+    print("hashed data", hashed_data)
+    print("decrypted data", decrypted_data)
 
-    if decrypted_data_hashed.lower() == hashed_data.lower():
+    if decrypted_data == hashed_data:
         print("Data integrity verified.")
     else:
         print("Data integrity compromised.")
